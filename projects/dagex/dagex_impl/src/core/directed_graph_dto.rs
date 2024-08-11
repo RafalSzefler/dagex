9d4302aca65c66746892a5d088a106321ad8d8aa
Rafal Szefler <rafael.szefler@gmail.com>
2024-08-11 18:00:17 +0200

    Used #[readonly]

--- projects/dagex/dagex_impl/src/core/directed_graph_dto.rs
+++ projects/dagex/dagex_impl/src/core/directed_graph_dto.rs
@@ -1,28 +1,14 @@
+use raf_readonly::readonly;
+
 /// Represents arrow between source node and target node in a directed graph.
 /// 
 /// # Notes
 /// Immutable once created.
+#[readonly]
 #[derive(PartialEq, Eq, Hash, Clone, Debug, Default)]
 pub struct ArrowDTO {
-    source: i32,
-    target: i32,
-}
-
-impl ArrowDTO {
-    #[inline(always)]
-    pub fn new(source: i32, target: i32) -> Self {
-        Self { source, target }
-    }
-
-    #[inline(always)]
-    pub fn source(&self) -> i32 {
-        self.source
-    }
-
-    #[inline(always)]
-    pub fn target(&self) -> i32 {
-        self.target
-    }
+    pub source: i32,
+    pub target: i32,
 }
 
 /// Represents directed graph as a pair consisting of number of nodes,
@@ -30,25 +16,9 @@ impl ArrowDTO {
 /// 
 /// # Notes
 /// Immutable once created.
+#[readonly]
 #[derive(PartialEq, Eq, Hash, Clone, Debug, Default)]
 pub struct DirectedGraphDTO {
-    number_of_nodes: i32,
-    arrows: Vec<ArrowDTO>,
-}
-
-impl DirectedGraphDTO {
-    #[inline(always)]
-    pub fn new(number_of_nodes: i32, arrows: Vec<ArrowDTO>) -> Self {
-        Self { number_of_nodes, arrows }
-    }
-
-    #[inline(always)]
-    pub fn number_of_nodes(&self) -> i32 {
-        self.number_of_nodes
-    }
-
-    #[inline(always)]
-    pub fn arrows(&self) -> &Vec<ArrowDTO> {
-        &self.arrows
-    }
+    pub number_of_nodes: i32,
+    pub arrows: Vec<ArrowDTO>,
 }
--- projects/dagex/dagex_impl/src/phylo/phylogenetic_network_dto.rs
+++ projects/dagex/dagex_impl/src/phylo/phylogenetic_network_dto.rs
@@ -1,28 +1,14 @@
 use std::collections::HashMap;
 
+use raf_readonly::readonly;
+
 use crate::raf_array::immutable_string::ImmutableString;
 
 use crate::core::DirectedGraphDTO;
 
+#[readonly]
 #[derive(PartialEq, Eq, Clone, Debug, Default)]
 pub struct PhylogeneticNetworkDTO {
-    graph: DirectedGraphDTO,
-    taxa: HashMap<i32, ImmutableString>,
-}
-
-impl PhylogeneticNetworkDTO {
-    #[inline(always)]
-    pub fn new(graph: DirectedGraphDTO, taxa: HashMap<i32, ImmutableString>) -> Self {
-        Self { graph, taxa }
-    }
-
-    #[inline(always)]
-    pub fn graph(&self) -> &DirectedGraphDTO {
-        &self.graph
-    }
-
-    #[inline(always)]
-    pub fn taxa(&self) -> &HashMap<i32, ImmutableString> {
-        &self.taxa
-    }
+    pub graph: DirectedGraphDTO,
+    pub taxa: HashMap<i32, ImmutableString>,
 }
